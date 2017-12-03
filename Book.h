75593bb61e2c13e970632f0deace0f52296af7cb
Will Murtagh <wmurtagh03@gmail.com>
2017-12-03 12:26:01 -0500

    Updated Book.h

--- Book.h
+++ Book.h
@@ -4,5 +4,20 @@
 
 #ifndef INC_220FINALPROJECT_BOOK_H
 #define INC_220FINALPROJECT_BOOK_H
+class Book{
+private:
 
+public:
+    virtual ~Book(){};
+    virtual std::string getAuthor()=0;
+    virtual std::string getTitle()=0;
+    virtual bool inInventory()=0;
+    virtual int HaveCount()=0;
+    virtual int WantCount()=0;
+    virtual int setPrice()=0;
+    virtual std::string getPrice()=0;
+
+
+
+};
 #endif //INC_220FINALPROJECT_BOOK_H
