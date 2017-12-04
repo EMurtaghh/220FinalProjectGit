ca25fd533a98d2e417bb40a0c5a68cd8a6b226fb
bkeith <bnkeith.20@gmail.com>
2017-12-03 20:37:38 -0500

    Got It!!!!!!!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "List.h"
 #include "ArrayList.h"
 #include "Bookstore.h"
@@ -9,6 +10,7 @@ int main() {
     while(choice!=2){
         std::cout<<"Enter 0 to add a book or 1 to print the Library or 2 to quit:"<<std::endl;
         std::cin>>choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         if(choice==0){
             myStore->addBook();
         }
